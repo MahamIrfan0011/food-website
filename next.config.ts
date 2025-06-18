402f38d1edec61ee75b9269fb0bd16e6596f95ef
MahamIrafm0011 <h.m.irfan553@gmail.com>
2025-06-19 00:36:03 +0500

    Fix

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,5 @@ module.exports = {
     domains: ['images.unsplash.com', 'images.pexels.com'],
   },
   
-  env: {
-    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
-    NEXT_PUBLIC_DOMAIN: process.env.NEXT_PUBLIC_DOMAIN,
-  },
 };
 
